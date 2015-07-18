15684df753b660cce420dd1a430e143ec47ffb08
Vinicius Jarina <viniciusjarina@gmail.com>
2015-07-18 00:07:00 -0300

    Adicionado GeradorDeDevedores utilizando HashSet

---
+++ Banco/Busca/GeradorDeDevedores.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco.Busca
+{
+	public class GeradorDeDevedores
+	{
+		public HashSet<string> GeraList()
+		{
+			HashSet<string> nomes = new HashSet<string> (); for (int i = 0 ; i < 30000 ; i++)
+				nomes.Add ("devedor " + i);
+			
+			return nomes;
+		}
+	}
+}
