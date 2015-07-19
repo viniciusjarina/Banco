36c79072a6284cf50310bdfb03189a5f4a4f4c57
Vinicius Jarina <viniciusjarina@gmail.com>
2015-07-19 16:27:17 -0300

    Adicionado arquivo pendente.

---
+++ Banco/Contas/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Banco.Contas
+{
+	public static class ObjectExtensions
+	{
+		public static string AsXml (this object resource) 
+		{
+			var stringWriter = new StringWriter();
+			new XmlSerializer (resource.GetType ()).Serialize (stringWriter, resource);
+			return stringWriter.ToString ();
+		}
+	}
+}
